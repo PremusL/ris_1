215122ab42663ff203d596ba08424f2becaa1669
premusl <luka1.premus2@gmail.com>
2024-03-19 23:29:03 +0100

    initial commit

---
+++ build/dis_tutorial3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/drevo2/rins/dis_tutorial3-main
+# Build directory: /home/drevo2/rins/dis_tutorial3-main/build/dis_tutorial3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ros2_commands.md
@@ -0,0 +1,4 @@
+## Ros2 commands
+
+ros2 launch dis_tutorial3 sim_turtlebot_nav.launch.py
+
